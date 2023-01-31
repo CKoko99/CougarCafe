24af3578e4781f7dd772bab784f72e1678c0cd66
koko2loko <courtney3koko@yahoo.com>
2023-01-31 10:08:38 -0600

    Add content for About Page

--- frontend/src/Components/About/About.js
+++ frontend/src/Components/About/About.js
@@ -2,12 +2,17 @@ import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 
-const aboutText = "Cougar Cafe is a Full Stack Web Application built using React, PHP, and MySQL"
+const aboutText1 = "Cougar Cafe is a Full Stack Web Application built using React, PHP, and MySQL"
+const aboutText2 = "The PHP backend uses REST APIs to send and receive data, store the data in a MySQL database, and retrieve it as needed."
+const aboutText3 = "The React frontend uses React Router to navigate between pages, and Material UI for the styling."
 export default function About() {
     return (
         <Box>
             <Typography variant="h2">About Cougar Cafe</Typography>
             <Typography variant="h4">Cougar Cafe is a Full Stack Web Application Built</Typography>
+            <Typography variant="h4">{aboutText1}</Typography>
+            <Typography variant="h4">{aboutText2}</Typography>
+            <Typography variant="h4">{aboutText3}</Typography>
         </Box>
     )
 }
